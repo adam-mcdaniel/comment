2093f01d7280d4980175f0927c57046cfe11a769
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-11-06 12:25:57 -0500

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+extern crate comment_strip;
+
+use comment_strip::{strip_comments, CommentStyle};
+use std::string::ToString;
+
+
+
+pub mod c {
+	use super::*;
+	pub fn strip(script: impl ToString) -> Result<String, &'static str> {
+		strip_comments(script.to_string(), CommentStyle::C, false)
+	}
+}
+
+pub mod cpp {
+	use super::*;
+	pub fn strip(script: impl ToString) -> Result<String, &'static str> {
+		strip_comments(script.to_string(), CommentStyle::C, false)
+	}
+}
+
+pub mod rust {
+	use super::*;
+	pub fn strip(script: impl ToString) -> Result<String, &'static str> {
+		strip_comments(script.to_string(), CommentStyle::C, false)
+	}
+}
+
+
+pub mod python {
+	use super::*;
+	pub fn strip(script: impl ToString) -> Result<String, &'static str> {
+                strip_comments(script.to_string(), CommentStyle::Shell, false)
+        }
+}
+
+pub mod shell {
+	use super::*;
+        pub fn strip(script: impl ToString) -> Result<String, &'static str> {
+                strip_comments(script.to_string(), CommentStyle::Shell, false)
+        }
+}
+
+pub mod xml {
+	use super::*;
+        pub fn strip(script: impl ToString) -> Result<String, &'static str> {
+                strip_comments(script.to_string(), CommentStyle::XML, false)
+        }
+}
+
+pub mod html {
+	use super::*;
+        pub fn strip(script: impl ToString) -> Result<String, &'static str> {
+                strip_comments(script.to_string(), CommentStyle::XML, false)
+        }
+}
